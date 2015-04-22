1df8be2a889e7b0895667ce5de8c7e4f95f9cc54
Gaston Aguilera <gaston.aguilera@globant.com>
2015-04-22 18:49:28 -0300

    url stream + metatags

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,28 +9,28 @@
  * Main module of the application.
  */
 angular
-  .module('malandracaSiteApp', [
+    .module('malandracaSiteApp', [
     'ngRoute',
     'ngAnimate',
     'ui.bootstrap-slider'
   ])
-  .value('soundManager', soundManager)
-  .constant('STREAM_URL','http://radio.pregonera.net:6366/1/;')
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/contact', {
-        templateUrl: 'views/contact.html',
-        controller: 'ContactCtrl'
-      })
-      .when('/radio', {
-        templateUrl: 'views/radio.html',
-        controller: 'RadioCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  })
+    .value('soundManager', soundManager)
+    .constant('STREAM_URL', 'http://stream.malandraca.com:6366/1/;')
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/contact', {
+                templateUrl: 'views/contact.html',
+                controller: 'ContactCtrl'
+            })
+            .when('/radio', {
+                templateUrl: 'views/radio.html',
+                controller: 'RadioCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    })
