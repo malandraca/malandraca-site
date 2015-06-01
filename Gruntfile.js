4f970dd55ab58231ab30e9634dcdd03432a7d370
napoo80 <magogaston@gmail.com>
2015-06-01 20:52:38 -0300

    song title

--- Gruntfile.js
+++ Gruntfile.js
@@ -69,6 +69,10 @@ module.exports = function (grunt) {
             options: {
                 base: 'dist',
                 branch: 'master',
+                user: {
+                    name: 'malandraca',
+                    email: 'admin@malandraca.com '
+                  },
                 repo: 'https://github.com/malandraca/malandraca.github.io.git'
             },
             src: ['**']
@@ -217,7 +221,7 @@ module.exports = function (grunt) {
                 src: [
           '<%= yeoman.dist %>/scripts/{,*/}*.js',
           '<%= yeoman.dist %>/styles/{,*/}*.css',
-          '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+          //'<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
           '<%= yeoman.dist %>/styles/fonts/*'
         ]
             }
@@ -249,7 +253,7 @@ module.exports = function (grunt) {
             options: {
                 assetsDirs: [
           '<%= yeoman.dist %>',
-          '<%= yeoman.dist %>/images',
+          //'<%= yeoman.dist %>/images',
           '<%= yeoman.dist %>/styles'
         ]
             }
